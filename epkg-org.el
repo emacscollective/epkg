bfe3535e97100aa65ff8245fd488d812a0d92e96
Jonas Bernoulli <jonas@bernoul.li>
2019-01-09 19:05:02 +0100

    Bump copyright years

--- epkg-org.el
+++ epkg-org.el
@@ -1,6 +1,6 @@
 ;;; epkg-org.el --- various Org mode utilities    -*- lexical-binding: t -*-
 
-;; Copyright (C) 2016-2018  Jonas Bernoulli
+;; Copyright (C) 2016-2019  Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 
