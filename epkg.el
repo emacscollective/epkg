58a63e5cc87c63e34f08508ab785f4bb4d3019cc
Jonas Bernoulli <jonas@bernoul.li>
2021-05-30 13:42:09 +0200

    Add SPDX-License-Identifier library header
    And rearrange library headers.

--- epkg.el
+++ epkg.el
@@ -4,22 +4,23 @@
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/emacscollective/epkg
-;; Package-Requires: ((closql "1.0.1") (emacs "25.1"))
 ;; Keywords: tools
-
-;; This file is not part of GNU Emacs.
+;; Package-Requires: ((closql "1.0.1") (emacs "25.1"))
+;; SPDX-License-Identifier: GPL-3.0-or-later
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published
 ;; by the Free Software Foundation; either version 3 of the License,
 ;; or (at your option) any later version.
-
+;;
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; For a full copy of the GNU GPL see http://www.gnu.org/licenses.
+
+;; This file is not part of GNU Emacs.
 
 ;;; Commentary:
 
