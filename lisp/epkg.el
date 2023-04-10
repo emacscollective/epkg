c5a29ca7cb816f94153efe3d9c48ab2b566f7506
Jonas Bernoulli <jonas@bernoul.li>
2023-04-10 21:28:29 +0200

    Bump emacsql and closql dependencies

--- lisp/epkg.el
+++ lisp/epkg.el
@@ -9,8 +9,8 @@
 ;; Package-Requires: (
 ;;     (emacs "25.1")
 ;;     (compat "29.1.3.4")
-;;     (closql "20230220")
-;;     (emacsql "20230220")
+;;     (closql "20230407")
+;;     (emacsql "20230409")
 ;;     (llama "0.2.0"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
