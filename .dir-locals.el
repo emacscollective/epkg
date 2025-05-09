327709a01a6eded82016a24c478456b8be31da09
Jonas Bernoulli <jonas@bernoul.li>
2025-05-09 16:42:07 +0200

    Update pull-request template

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,11 @@
 ((nil
   (indent-tabs-mode . nil))
- (".github"
-  (nil . ((truncate-lines . nil)))))
+ (makefile-mode
+  (indent-tabs-mode . t))
+ (git-commit-mode
+  (git-commit-major-mode . git-commit-elisp-text-mode))
+ (".github/PULL_REQUEST_TEMPLATE"
+  (nil (truncate-lines . nil)))
+ ("CHANGELOG"
+  (nil (fill-column . 70)
+       (mode . display-fill-column-indicator))))
