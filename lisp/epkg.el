a94d4b0ff889c20f059b9ac9848369741d115a96
Jonas Bernoulli <jonas@bernoul.li>
2026-08-01 00:51:02 +0200

    Release version 4.2.3

--- lisp/epkg.el
+++ lisp/epkg.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/emacscollective/epkg
 ;; Keywords: tools
 
-;; Package-Version: 4.2.2
+;; Package-Version: 4.2.3
 ;; Package-Requires: (
 ;;     (emacs   "28.1")
 ;;     (compat  "31.0")
